62ea1b2276eafe87f5bd954f82bc8cd38bc72fcf
burcinsari <burcin.sari@std.yeditepe.edu.tr>
2026-01-06 15:45:27 +0300

    Implement fetchTasks to call API and map DTOs

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,22 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val tasksDto = api.getTasks()
+
         // TODO 2: Convert TaskDto list to Task list
+        return tasksDto.map { taskDto ->
+            Task(
+                id = taskDto.id,
+                title = taskDto.title,
+                completed = taskDto.completed,
+                userId = taskDto.userId
+            )
+        }
+
+
+
         return TODO("Provide the return value")
+
     }
 }
+
