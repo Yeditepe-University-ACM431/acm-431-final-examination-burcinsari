eea2c23f7510c1b05b8458546ef239cc2b3e18ba
burcinsari <burcin.sari@std.yeditepe.edu.tr>
2026-01-06 15:47:47 +0300

    Implement completion status in TaskItemScreen
    Added mutable state for completion status and updated UI.

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
@@ -16,16 +16,16 @@ fun TaskItemScreen() {
     // TODO 1: Create a mutable state to hold completion status (Boolean)
     // Initial value should be false
 
+    var isCompleted by remember { mutableStateOf(false) }
     Column(modifier = Modifier.padding(16.dp)) {
-
         Text(
             text = "Submit Final Project",
             style = MaterialTheme.typography.titleLarge
         )
-
         // TODO 2: Show text "Completed" or "Not Completed"
         // depending on completion state
 
+        Text(text = if (isCompleted) "Completed" else "Not Completed")
         Button(
             onClick = {
                 // TODO 3: Toggle completion state
@@ -40,4 +40,5 @@ fun TaskItemScreen() {
 @Composable
 fun TaskItemPreview() {
     // TODO: Call TaskItemScreen
+    TaskItemScreen()
 }
